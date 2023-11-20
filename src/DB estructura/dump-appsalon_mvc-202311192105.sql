dd6e9c337320e6f22e7551ca45cf1ec62c1a2898
EdissonAcero <109987805+EdissonFrancisco@users.noreply.github.com>
2023-11-19 21:36:35 -0500

    Add files via upload

---
+++ src/DB estructura/dump-appsalon_mvc-202311192105.sql
@@ -0,0 +1,142 @@
+-- MySQL dump 10.13  Distrib 8.1.0, for Win64 (x86_64)
+--
+-- Host: localhost    Database: appsalon_mvc
+-- ------------------------------------------------------
+-- Server version	8.1.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `citas`
+--
+
+DROP TABLE IF EXISTS `citas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `citas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fecha` date DEFAULT NULL,
+  `hora` time DEFAULT NULL,
+  `usuarioId` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usuarioId_idx` (`usuarioId`),
+  CONSTRAINT `citas_ibfk_1` FOREIGN KEY (`usuarioId`) REFERENCES `usuarios` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `citas`
+--
+
+LOCK TABLES `citas` WRITE;
+/*!40000 ALTER TABLE `citas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `citas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `citasservicios`
+--
+
+DROP TABLE IF EXISTS `citasservicios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `citasservicios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `citaId` int DEFAULT NULL,
+  `servicioId` int DEFAULT NULL,
+  KEY `ServicioId` (`id`),
+  KEY `citaId` (`citaId`),
+  KEY `servicioId_2` (`servicioId`),
+  CONSTRAINT `citasservicios_ibfk_2` FOREIGN KEY (`citaId`) REFERENCES `citas` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
+  CONSTRAINT `citasservicios_ibfk_3` FOREIGN KEY (`servicioId`) REFERENCES `citasservicios` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `citasservicios`
+--
+
+LOCK TABLES `citasservicios` WRITE;
+/*!40000 ALTER TABLE `citasservicios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `citasservicios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `servicios`
+--
+
+DROP TABLE IF EXISTS `servicios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `servicios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(60) NOT NULL,
+  `precio` decimal(8,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `servicios`
+--
+
+LOCK TABLES `servicios` WRITE;
+/*!40000 ALTER TABLE `servicios` DISABLE KEYS */;
+INSERT INTO `servicios` VALUES (1,'Corte de cabello mujer',28000.00),(2,'Corte de cabello hombre',15000.00),(3,'Corte de cabello niño',10000.00),(4,'Peinado mujer',50000.00),(5,'Peinado hombre',23000.00),(6,'peinado niño',15000.00),(7,'Corte de barba',12000.00),(8,'Tinte mujer',32000.00),(9,'Uñas acrilico',60000.00),(10,'Uñas esmalte y decoracion',25000.00),(11,'Lavado de cabello',15000.00),(12,'Tratamiento capilar',48000.00);
+/*!40000 ALTER TABLE `servicios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(60) NOT NULL,
+  `apellido` varchar(60) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `telefono` varchar(10) NOT NULL,
+  `admin` tinyint(1) DEFAULT NULL,
+  `password` varchar(60) DEFAULT NULL,
+  `confirmado` tinyint(1) DEFAULT NULL,
+  `token` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'Edisson','Acero','correo@correo.com','3013339000',0,'$2y$10$2DVIgORXEZc5ZNkqrC4mfOQlEDCml1vXvzfyr49ngyudiaMR6O/HG',1,''),(2,'Diego','Carreño','email@correo.com','3216549872',0,'$2y$10$UouQH0ZMgUCc0PRMlWMnreD6S99bj.ClU6h52Uya4Ep90yUG7n7la',1,''),(3,'Camilo','Camargo','camilo@correo.com','4563217890',0,'$2y$10$SWFhJuPuL4T//E9XGbqJguJXgm0SG.wpEvgDhlrGRDethdDvAjfum',1,''),(4,'Oskar','Petro','op@correo.com','3012587469',0,'$2y$10$TGF0eOgs8ceUP517tNJQ5e1Cr2sPy/IJ8Lc2F1cdo.T.66/y6ZpRm',1,'');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'appsalon_mvc'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-19 21:05:04
