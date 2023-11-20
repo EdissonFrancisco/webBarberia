47da9b08d49996a624a76c0e3450888093aec3f8
EdissonAcero <109987805+EdissonFrancisco@users.noreply.github.com>
2023-11-19 21:42:01 -0500

    bd corregida

--- src/DB estructura/appsalon_mvc_DDL.sql
+++ src/DB estructura/appsalon_mvc_DDL.sql
@@ -57,7 +57,7 @@ CREATE TABLE `citasservicios` (
   KEY `citaId` (`citaId`),
   KEY `servicioId_2` (`servicioId`),
   CONSTRAINT `citasservicios_ibfk_2` FOREIGN KEY (`citaId`) REFERENCES `citas` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
-  CONSTRAINT `citasservicios_ibfk_3` FOREIGN KEY (`servicioId`) REFERENCES `citasservicios` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
+  CONSTRAINT `citasservicios_ibfk_3` FOREIGN KEY (`servicioId`) REFERENCES `servicios` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
